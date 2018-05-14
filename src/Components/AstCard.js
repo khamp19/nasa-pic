6c2f021cc694e0ea6f031b6e3f8055b4244f4611
khamp19 <khampton119@gmail.com>
2018-05-14 19:21:26 -0400

    updating css

--- src/Components/AstCard.js
+++ src/Components/AstCard.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 const AstCard = (props) => {
-  const { title, url, hdurl, explanation, date, copyright } = props.data; //object destructuring!! passes each part of the props object to the card
+  const { title, url, hdurl, explanation, date, copyright } = props.data; 
   return (
     <div className="card">
-      <h2>NASA Image of the Day</h2>
-      <h4>{title}</h4>
+      <h1>NASA Image of the Day</h1>
+      <h2>{title}</h2>
+      <p>{date}</p>
       <a href={hdurl} className="wrapper">
         <img src={url} alt={title}/> 
       </a>
-      <p>{explanation}</p>
-      <span>{date}, {copyright}</span>
+      <div className="info">
+        <p>{explanation}</p>
+        <p>copyright: {copyright}</p>
+      </div>
     </div>
   )
 }
